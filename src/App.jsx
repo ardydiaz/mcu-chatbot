6252fd526499c608b6b5e8bc37c0e7b7d25aa23a
ardydiaz <ardywerkz19@gmail.com>
2026-03-18 14:44:48 +0800

    update code 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+
 import ChatbotIcon from "./component/ChatbotIcon";
 import ChatForm from "./component/ChatForm";
 import ChatMessage from "./component/ChatMessage";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // Get env variables
 const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
@@ -10,6 +10,8 @@ const GEMINI_URL = import.meta.env.VITE_GEMINI_URL;
 
 const App = () => {
   const [chatHistory, setChatHistory] = useState([]);
+  const [showChatbot, setShowChatbot] = useState(false);
+  const chatBodyRef = useRef();
   
   //helper function to update chat history
   const generateBotResponse = async(history) => {
@@ -33,30 +35,41 @@ const App = () => {
       const response = await fetch(GEMINI_URL, requestBody);
       const data = await response.json();
       if(!response.ok) throw new Error(data.error.message || "Failed to generate response");  
-      //return data;
-      const apiResponse = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1' ).trim(); //extracting the bot response from the API response
+      const apiResponse = data.candidates[0].content.parts[0].text.replace(/#{1,6}\s?/g, '') // remove headings (###, ####)
+            .replace(/\*\*(.*?)\*\*/g, '$1')      // remove bold **
+            .replace(/\*(.*?)\*/g, '$1')          // remove italic *
+            .replace(/\n{2,}/g, '\n').trim(); //extracting the bot response from the API response
       //update the chat history with the bot response
       updateHistory(apiResponse);
     } catch (error) {
       console.error("Error generating bot response:", error);
       //setChatHistory((history) => [...history, { role: "model", text: "Sorry, something went wrong. Please try again later." }]);
     }
-  }
+  };
+
+  useEffect(() => {
+    //scroll to the bottom of the chat body whenever the chat history is updated
+    chatBodyRef.current.scrollTo({ top: chatBodyRef.current.scrollHeight, behavior: "smooth" });
+  }, [chatHistory]);
 
   return (
-    <div className="container">
+    <div className={`container ${showChatbot ? 'show-chatbot' : ''}`}> {/*toggle the show-chatbot class based on the showChatbot state*/} 
+      <div onClick={() => setShowChatbot(prev => !prev)} id="chatbot-toggler">
+        <span className="material-symbols-rounded">mode_comment</span>
+        <span className="material-symbols-rounded">close</span>
+      </div>
       <div className="chatbot-popup">
         {/*Chat bot header content */}
          <div className="chat-header">
            <div className="header-info">
             <ChatbotIcon />
-             <h2 className="logo-text">Chatbot</h2>
+             <h2 className="logo-text">MCU CHATBOT</h2>
            </div>
-           <button className="material-symbols-rounded">keyboard_arrow_down</button>
+           <button className="material-symbols-rounded" onClick={() => setShowChatbot(prev => !prev)}>keyboard_arrow_down</button>
          </div>
 
           {/*Chat bot body content */}
-         <div className="chat-body">
+         <div ref={chatBodyRef} className="chat-body">
            <div className="message bot-message">
               <ChatbotIcon />
               <p className="message-text">Hello! How can I assist you today?</p>
